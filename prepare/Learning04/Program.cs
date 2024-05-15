16827039cb9eb1227a546fcb8c084fbc4cca16a6
Ryan Earwood <ear24003@byui.edu>
2024-05-15 14:20:03 -0400

    1st

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+
+        Assignment assignment = new Assignment("John Doe", "Mathematics");
+
+
+        string summary = assignment.GetSummary();
+
+
+        Console.WriteLine(summary);
+
     }
+
+
 }
